8ade7dc4fd40d11f62df96df00ed2484bfa8dd73
Stevo <steven.barrios92@gmail.com>
2023-02-15 16:57:00 -0800

    files for layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+import soon from './comingsoon.jpeg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={soon} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
