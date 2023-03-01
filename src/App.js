fbb0e9ba818e6235df75a502ad58cabe6008803e
Stevo <steven.barrios92@gmail.com>
2023-02-28 21:14:05 -0800

    starter files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import soon from './comingsoon.jpeg'
-import './App.css';
+import React from "react";
+import Home from "./components/homepage"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={soon} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Home />
     </div>
   );
 }
---
+++ src/components/homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FrontRest from "../img/FrontRes.jpg"
+import { Parallax } from 'react-parallax';
+
+function Container() {
+    <Parallax className='bgImage' bgImage={FrontRest}>
+        <div className='contentHome'>
+        hello
+        </div>
+    </Parallax>
+};
+
+export default Container;
