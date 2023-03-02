00d0e7788435024575684de7056f62c69e17f0b2
Stevo <steven.barrios92@gmail.com>
2023-03-01 20:02:44 -0800

    added nav & images,tailwind, working on home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Home from "./components/homepage"
+import './index.css'
 
 function App() {
   return (
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import FrontRest from "../img/FrontRes.jpg"
 import { Parallax } from 'react-parallax';
+import Nav from "./nav"
 
-function Container() {
-    <Parallax className='bgImage' bgImage={FrontRest}>
-        <div className='contentHome'>
-        hello
-        </div>
-    </Parallax>
+function Home() {
+    return (
+        <Parallax className='bgImage' bgImage={FrontRest} strength={800}>
+            <Nav />
+            <div className='contentHome'>
+
+                <h1>Welcome To my resturant</h1>
+
+            </div>
+        </Parallax>
+    )
 };
 
-export default Container;
+export default Home;
