637647cd177146af4f3bc858ef38bea633098c0f
Stevo <steven.barrios92@gmail.com>
2023-03-20 19:25:40 -0700

    changed button name

--- components/contactForm.js
+++ components/contactForm.js
@@ -9,7 +9,7 @@ function Contact() {
         <div class="mx-auto max-w-2xl py-96">
           <div class="text-center">
             <div class="mt-auto flex items-center justify-center gap-x-6">
-              <Link href="/menu" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Next Page</Link>
+              <Link href="/menu" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Menu Page</Link>
             </div>
           </div>
         </div>
