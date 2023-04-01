07ac1411ee31272d2d7e762b426cf2afbeea13c1
Stevo <steven.barrios92@gmail.com>
2023-03-31 21:11:04 -0700

    fixed one part of the cards, need to only show only 3

--- components/happyhour.js
+++ components/happyhour.js
@@ -6,7 +6,6 @@ function HappyHr() {
 
     const displayFood = happyHR.map((k, index) => {
         return (
-        <div className="grid grid-cols-3 gap-4 text-brown text-center py-20">
             <div className="w-80 mt-24 m-auto lg:mt-16 max-w-sm">
                 <img src={k.image} alt="" class="rounded-t-2xl shadow-2xl lg:w-full 2xl:w-full 2xl:h-44 object-cover" />
                 <div className="bg-white shadow-2xl rounded-b-3xl">
@@ -16,7 +15,6 @@ function HappyHr() {
                     </div>
                 </div>
             </div>
-        </div>
         )
     }
         )
@@ -24,7 +22,9 @@ function HappyHr() {
 return (
     <>
         <h1 className="text-center text-white pt-8 text-5xl">Happy Hour Section</h1>
+        <div className="grid grid-cols-3 gap-4 text-brown text-center py-20">
         {displayFood}
+        </div>
     </>
 )
 }
