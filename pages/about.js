424aa3de74f31e47a379d5b28be40808a39ee731
Stevo <steven.barrios92@gmail.com>
2023-05-06 13:49:39 -0700

    changes to about:

--- pages/about.js
+++ pages/about.js
@@ -71,33 +71,6 @@ function about() {
                                 <p className="text-white">Line Cook</p>
                             </div>
 
-                            <div className="mb-12">
-                                <img src="https://mdbootstrap.com/img/new/avatars/12.jpg" class="rounded-full mx-auto shadow-lg mb-4" alt=""
-                                />
-                                <p className="font-bold mb-2">Avaya Hills</p>
-                                <p className="text-white">Host</p>
-                            </div>
-
-                            <div className="mb-12">
-                                <img src="https://mdbootstrap.com/img/new/avatars/13.jpg" class="rounded-full mx-auto shadow-lg mb-4" alt=""
-                                />
-                                <p className="font-bold mb-2">Thierry Fischer</p>
-                                <p className="text-white">Barback</p>
-                            </div>
-
-                            <div className="mb-12">
-                                <img src="https://mdbootstrap.com/img/new/avatars/14.jpg" class="rounded-full mx-auto shadow-lg mb-4" alt=""
-                                />
-                                <p className="font-bold mb-2">Aisling Sheldon</p>
-                                <p className="text-white">Cashier</p>
-                            </div>
-
-                            <div className="mb-12">
-                                <img src="https://mdbootstrap.com/img/new/avatars/15.jpg" class="rounded-full mx-auto shadow-lg mb-4" alt=""
-                                />
-                                <p className="font-bold mb-2">Ayat Black</p>
-                                <p className="text-white">Dishwasher</p>
-                            </div>
                         </div>
                     </div>
 
