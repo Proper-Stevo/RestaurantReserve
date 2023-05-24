64f00c92872295d8bd09fc2f4e0e6600742abd8d
Steven Barrios <steven.barrios92@gmail.com>
2023-05-24 15:43:55 -0700

    Update reservations.js

--- pages/api/reservations.js
+++ pages/api/reservations.js
@@ -1,61 +1,87 @@
-const reservationNumber = Math.floor(Math.random() * 100000);
-const nodemailer = require("nodemailer");
-require("dotenv").config();
-const fs = require("fs");
-const path = require("path");
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
+
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.USER,
+    pass: process.env.PASS,
+  },
+});
+
+async function sendReservationConfirmationEmail(
+  guests,
+  names,
+  date,
+  time,
+  email,
+  reservationNumber
+) {
+  const imagePath = path.join(__dirname, "/public/images/borderblk.png");
+  const resFrontPath = path.join(
+    __dirname,
+    "../../../../public/images/ResFront.png"
+  );
+
+  const mailOptions = {
+    from: "South Central With Love <laresturaunt@gmail.com>",
+    to: email,
+    subject: "Reservation Confirmation",
+    html: `<body style="background-image: url('cid:borderblk');background-repeat: no-repeat;background-size: cover;">
+      <div style="margin: 200px;">
+        <h1 style="text-align: center;">Thanks ${names}!</h1>
+        <h3 style="text-align: center;">Your reservation for ${guests} guests on ${date} at ${time}.</h3>
+        <br />
+        <h3 style="text-align: center;">Your reservation number is ${reservationNumber}.</h3>
+        <br />
+        <h1 style="text-align: center;">South Central, With Love</h1>
+        <h4 style="text-align: center;">Look forward to seeing you soon!!</h4>
+        <center>
+          <img src="cid:ResFont" alt="Restaurant" width="400px" height="400px" class="center" />
+        </center>
+      </div>
+    </body>`,
+    attachments: [
+      {
+        filename: "borderblk.png",
+        path: imagePath,
+        cid: "borderblk",
+        contentDisposition: "inline",
+        contentType: "image/png",
+      },
+      {
+        filename: "ResFont.png",
+        path: resFrontPath,
+        cid: "ResFont",
+        contentDisposition: "inline",
+        contentType: "image/png",
+      },
+    ],
+  };
+
+  await transporter.sendMail(mailOptions);
+}
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const { guests, names, date, time, email } = req.body;
-    const imagePath = path.join(__dirname, "/public/images/borderblk.png");
-    const resFrontPath = path.join(__dirname, "../../../../public/images/ResFront.png");
+    const reservationNumber = Math.floor(Math.random() * 100000);
 
     try {
-      const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-          user: process.env.USER,
-          pass: process.env.PASS,
-        },
-      });
-
-      await transporter.sendMail({
-        from: "South Central With Love <laresturaunt@gmail.com>",
-        to: email,
-        subject: "Reservation Confirmation",
-        html: `<body style="background-image: url('cid:borderblk');background-repeat: no-repeat;background-size: cover;">
-          <div style="margin: 200px;">
-            <h1 style="text-align: center;">Thanks ${names}!</h1>
-            <h3 style="text-align: center;">Your reservation for ${guests} guests on ${date} at ${time}.</h3>
-            <br />
-            <h3 style="text-align: center;">Your reservation number is ${reservationNumber}.</h3>
-            <br />
-            <h1 style="text-align: center;">South Central, With Love</h1>
-            <h4 style="text-align: center;">Look forward to seeing you soon!!</h4>
-            <center>
-              <img src="cid:ResFont" alt="Restaurant" width="400px" height="400px" class="center" />
-            </center>
-          </div>
-        </body>`,
-        attachments: [
-          {
-            filename: "borderblk.png",
-            path: imagePath,
-            cid: "borderblk",
-            contentDisposition: "inline",
-            contentType: "image/png",
-          },
-          {
-            filename: "ResFont.png",
-            path: resFrontPath,
-            cid: "ResFont",
-            contentDisposition: "inline",
-            contentType: "image/png",
-          },
-        ],
-      });
+      await sendReservationConfirmationEmail(
+        guests,
+        names,
+        date,
+        time,
+        email,
+        reservationNumber
+      );
 
       return res.status(200).json({ reservationNumber });
     } catch (error) {
