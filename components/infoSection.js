c45c1b8514c43b9f321574d543efccbd42c475d1
Stevo <steven.barrios92@gmail.com>
2023-06-12 19:14:53 -0700

    no more parrallax

--- components/infoSection.js
+++ components/infoSection.js
@@ -5,7 +5,7 @@ function Info() {
     return (
         <>
             <Parallax className="tacosBG" bgImage="/images/tacos.png">
-                <div className="grid grid-cols-3 gap-4 text-brown text-center py-20">
+                <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 text-brown text-center py-20">
                 <div>
                     <br />
                 <img className="mx-auto" src="/images/addy.png" />
@@ -32,7 +32,7 @@ function Info() {
                 <img className="mx-auto" src="/images/contact.png" />
                 <b>Contact:</b>
                  <br />
-                    Email: Steven.barrios92@gmail.com <br /> 
+                    Email: Steven.barrios92@gmail.com<br /> 
                     Phone: 323.719.0378
                 </div>
                 </div>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
-import { Parallax } from 'react-parallax';
 import Info from '../components/infoSection';
 import Bar from '../components/bar';
 import Story from '../components/backStory';
@@ -12,15 +11,15 @@ export default function Home() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <Parallax className='bgImage' bgImage="/images/FrontRes.jpg" strength={900}>
+      <div id='home'>
         <div className="relative mx-auto max-w-2xl pt-32 pb-48 sm:py-48 lg:py-56">
           <div className="relative sm:mb-8 sm:flex sm:justify-center">
-              <div className="fixed text-center text-6xl font-bold tracking-tight text-white sm:text-6xl md:text-7xl lg:text-9xl">South Central,<br /> With Love.
+            <div className="fixed text-center text-6xl font-bold tracking-tight text-white sm:text-6xl md:text-7xl lg:text-9xl">South Central,<br /> With Love.
               <p className="mt-6 text-2xl leading-8 text-white tracking-normal  ">Los Angeles Cuisine, from the heart of California.</p>
-              </div>
+            </div>
           </div>
         </div>
-      </Parallax>
+        </div>
       <Info />
       <Bar />
       <Story />
